8548071b5056b5d14c9defd0d1289f7f57bea92f
leviSunesson <levi@sunesson.com>
2018-08-30 10:52:21 +0200

    Uppgift 1

---
+++ src/v35/Uppgift1.java
@@ -0,0 +1,23 @@
+package v35;
+
+public class Uppgift1 {
+
+	public static void main(String[] args) {
+		
+		sum(1, 1, 1, 2, 3, 4, 5, 6, 7);
+		
+	}
+	
+	public static int sum(int... ints) {
+		
+		int sum = 0;
+		
+		for (int integ : ints) {
+			sum += integ;
+		}
+		
+		return sum;
+		
+	}
+	
+}
