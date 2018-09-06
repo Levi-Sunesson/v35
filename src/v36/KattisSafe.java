07ca32ddc34d4f45e407e1ec85a6f7392061c9b5
leviSunesson <levi@sunesson.com>
2018-09-06 10:40:11 +0200

    Created a method for checking if the grid is solved

--- src/v36/KattisSafe.java
+++ src/v36/KattisSafe.java
@@ -15,6 +15,7 @@ public class KattisSafe {
 
 		int[][] grid = new int[SIZE][SIZE];
 
+		//Fills the entire grid with the numbers from the console
 		for (int x = 0; x < SIZE ; x++) {
 			for (int y = 0; y < SIZE; y++) {
 
@@ -23,22 +24,65 @@ public class KattisSafe {
 			}
 		}
 
-		System.out.println("win");
+		press(grid, 1, 1, 1);
+
+		isSolved(grid);
+		
+		for (int x = 0; x < SIZE ; x++) {
+			for (int y = 0; y < SIZE; y++) {
+				System.out.print(grid[x][y]);
+			}
+			System.out.println("");
+		}
 
 	}
+	/**
+	 * This method presses the buttons
+	 * accord to the rules in the assignment
+	 * 
+	 * @param grid The grid
+	 * @param row What row is the pressed button
+	 * @param col What column is the pressed button
+	 * @param timesPressed Number of times the button is pressed
+	 */
+	private static void press(int[][] grid, int row, int col, int timesPressed) {
 
-	static void press(int[][] grid, int row, int col, int timespressed) {
+		//Checks the entire grid
+		for (int x = 0; x < SIZE ; x++) {
+			for (int y = 0; y < SIZE; y++) {
 
+				// If the coordinate we are looking at is the same as the pressed one then
+				if (x == col || y == row) { 
 
+					// Increase the number according to the RANGE
+					grid[x][y] = (grid[x][y] + timesPressed + RANGE) % RANGE;
+
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method checks if the grid is only filled with zeroes or not
+	 * 
+	 * @param grid The grid to be checked
+	 * @return True if solved and false if not
+	 */
+	private static boolean isSolved(int[][] grid) {
+
+		//Checks the entire grid
 		for (int x = 0; x < SIZE ; x++) {
 			for (int y = 0; y < SIZE; y++) {
 				
-				if (x == col || y == row) {
-					
-				}
+				//If a coordinate is not 0 then it returns false
+				if (grid[x][y] != 0)
+					return false;
 				
 			}
 		}
+
+		//Returns true
+		return true;
 
 	}
 
